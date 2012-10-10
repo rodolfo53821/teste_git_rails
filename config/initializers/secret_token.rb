1009c481d6462202ead63416b0e047ef8c833681
Rodolfo <rodolfo53821@hotmail.com>
2012-10-10 18:06:59 -0300

    Projeto rails criado no shell do linux

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TesteGitRails::Application.config.secret_token = '7c2bba2931df3994c2ab12595628e83dca2c52f9a491ba3cb3aba276f0ea8b725a0f2434f9a8cdac5edf3d46d9088011fc6c0e217efe0212bdad9e0301ee9633'
